e693e356ce21a377aca7e8819a51de3188a6d9b8
lbackman <lucasbackman@gmail.com>
2023-04-12 16:27:08 +0300

    Add friend request model

---
+++ db/migrate/20230412131418_create_friend_requests.rb
@@ -0,0 +1,11 @@
+class CreateFriendRequests < ActiveRecord::Migration[7.0]
+  def change
+    create_table :friend_requests do |t|
+      t.references :sender, null: false, foreign_key: {to_table: :users}
+      t.references :receiver, null: false, foreign_key: {to_table: :users}
+      t.boolean    :accepted, default: nil
+
+      t.timestamps
+    end
+  end
+end
