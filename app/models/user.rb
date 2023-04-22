c6f08c15a676c7b3bd3469e3e28026fd4c8dd4f0
lbackman <lucasbackman@gmail.com>
2023-04-22 16:37:12 +0300

    Create mutual friendships in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,36 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Friendships
+
+  has_many :sent_friend_requests,
+           class_name: "FriendRequest",
+           foreign_key: :sender_id
+
+  has_many :friendships_as_sender,
+           -> { where("accepted = ?", true) },
+           class_name: "FriendRequest",
+           foreign_key: :sender_id
+      
+  has_many :sent_friends,
+           through: :friendships_as_sender,
+           source: :receiver
+
+  has_many :received_friend_requests,
+           class_name: "FriendRequest",
+           foreign_key: :receiver_id         
+
+  has_many :friendships_as_receiver,
+           -> { where("accepted = ?", true) },
+           class_name: "FriendRequest",
+           foreign_key: :receiver_id
+
+  has_many :received_friends,
+           through: :friendships_as_receiver,
+           source: :sender
+
+  def friends
+    sent_friends + received_friends
+  end
 end
