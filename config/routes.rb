d1ff505b1eba011d12af986d1c96c4fccddf9f9f
lbackman <lucasbackman@gmail.com>
2023-04-24 11:57:13 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :users, except: [:new, :create]
+  resources :friend_requests, only: [:index, :show, :create, :update, :destroy]
+
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
 end
