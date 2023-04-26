2c54db5202f54f5d14d04ebbf70627d0fd7f9008
lbackman <lucasbackman@gmail.com>
2023-04-26 17:26:36 +0300

    Move duplication to before_action

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,10 +1,11 @@
 class FriendRequestsController < ApplicationController
+  before_action :set_friend_request, only: [:show, :update, :destroy]
+
   def index
     @friend_requests = FriendRequest.all
   end
 
   def show
-    @friend_request = FriendRequest.find_by_id(params[:id])
   end
 
   def create
@@ -12,16 +13,18 @@ class FriendRequestsController < ApplicationController
   end
 
   def update
-    @friend_request = FriendRequest.find_by_id(params[:id])
     @friend_request.update(accepted: true)
   end
 
   def destroy
-    @friend_request = FriendRequest.find_by_id(params[:id])
     @friend_request.destroy
   end
 
   private
+
+    def set_friend_request
+      @friend_request = FriendRequest.find_by_id(params[:id])
+    end
 
     def friend_request_params
       params.require(:friend_request).permit(:sender_id, :receiver_id)
