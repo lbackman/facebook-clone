ccb890456e0024c2baf41580c72a827d2885ed04
lbackman <lucasbackman@gmail.com>
2023-05-23 17:54:14 +0300

    Associate friend request with notification

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -2,6 +2,9 @@ class FriendRequest < ApplicationRecord
   belongs_to :sender, class_name: "User"
   belongs_to :receiver, class_name: "User"
 
+  after_create_commit :notify_receiver
+  has_noticed_notifications # removes notification when deleting friend request
+
   validates :sender_id, :receiver_id,
             presence: true,
             unique_friend_request: true, on: :create
@@ -10,4 +13,10 @@ class FriendRequest < ApplicationRecord
 
   scope :sent, ->(user) { FriendRequest.where(sender: user, accepted: false) }
   scope :received, ->(user) { FriendRequest.where(receiver: user, accepted: false) }
+
+  private
+
+  def notify_receiver
+    FriendRequestNotification.with(friend_request: self).deliver_later(receiver)
+  end
 end
