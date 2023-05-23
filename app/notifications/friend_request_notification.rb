aa1c7da5211870f8d1321de7f6a89c2d9f8cd402
lbackman <lucasbackman@gmail.com>
2023-05-23 17:52:31 +0300

    Create notifications for friend request

---
+++ app/notifications/friend_request_notification.rb
@@ -0,0 +1,28 @@
+# To deliver this notification:
+#
+# FriendRequestNotification.with(friend_request: @friend_request).deliver_later(friend_request.receiver)
+# FriendRequestNotification.with(post: @post).deliver(current_user)
+
+class FriendRequestNotification < Noticed::Base
+  # Add your delivery methods
+  #
+  deliver_by :database
+  # deliver_by :action_cable
+  # deliver_by :email, mailer: "UserMailer"
+  # deliver_by :slack
+  # deliver_by :custom, class: "MyDeliveryMethod"
+
+  # Add required params
+  #
+  param :friend_request
+
+  # Define helper methods to make rendering easier.
+  #
+  def message
+    t(".message", sender: User.find(params[:friend_request][:sender_id]).first_name)
+  end
+  #
+  def url
+    user_friend_requests_path(User.find(params[:friend_request][:receiver_id]))
+  end
+end
