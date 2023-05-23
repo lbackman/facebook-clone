9a37683e56d18fca5c9ca2c771870e0039df5655
lbackman <lucasbackman@gmail.com>
2023-05-23 17:55:41 +0300

    Create dropdown for notifications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_notifications, if: :current_user
 
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
@@ -7,5 +8,13 @@ class ApplicationController < ActionController::Base
 
   def render_access_denied
     render :file => "#{Rails.root}/public/403.html",  :status => 403, layout: false, status: :forbidden
+  end
+
+  private
+
+  def set_notifications
+    notifications = Notification.where(recipient: current_user).newest_first.limit(9)
+    @unread = notifications.unread
+    @read = notifications.read
   end
 end
