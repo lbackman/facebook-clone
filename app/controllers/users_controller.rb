3c4ad4f2c612f405bf0b181f3bba5b1c6e2c5200
lbackman <lucasbackman@gmail.com>
2023-05-31 10:22:30 +0300

    Create mutual scope for friend request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by_id(params[:id])
-    @friend_request = FriendRequest.where(
-      sender:   [current_user, @user],
-      receiver: [current_user, @user]
-    ).first || FriendRequest.new
+    @friend_request = FriendRequest.mutual(current_user, @user).first || FriendRequest.new
 
     if @user != current_user && @friend_request.created_at
       mark_notifications_as_read(@friend_request.notifications_as_friend_request.where(recipient: current_user))
--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -13,6 +13,10 @@ class FriendRequest < ApplicationRecord
 
   scope :sent, ->(user) { FriendRequest.where(sender: user, accepted: false) }
   scope :received, ->(user) { FriendRequest.where(receiver: user, accepted: false) }
+  scope :mutual, ->(user_1, user_2) {
+    FriendRequest.where(
+      sender:   [user_1, user_2],
+      receiver: [user_1, user_2]) }
 
   private
 
