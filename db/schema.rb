23eff25c18a2080ca5f193547f8b7cc7e3c76e6b
lbackman <lucasbackman@gmail.com>
2023-06-10 11:59:07 +0300

    Use cache counter for like count

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_07_060615) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_10_085603) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_060615) do
     t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "likes_count"
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
