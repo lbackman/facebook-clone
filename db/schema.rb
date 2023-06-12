4d2c3a79706b447ce7b5b51208594b97dbc6bdb4
lbackman <lucasbackman@gmail.com>
2023-06-12 15:08:01 +0300

    Allow author_id to be null for comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_12_063158) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_12_115449) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.text "body", null: false
-    t.bigint "author_id", null: false
+    t.bigint "author_id"
     t.string "commentable_type", null: false
     t.bigint "commentable_id", null: false
     t.datetime "created_at", null: false
