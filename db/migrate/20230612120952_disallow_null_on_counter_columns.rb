35d5b30635999fac4c40f678ec7f760d9265b797
lbackman <lucasbackman@gmail.com>
2023-06-12 15:17:59 +0300

    Allow counter caches to be null
    Won't have to check if count exists first

---
+++ db/migrate/20230612120952_disallow_null_on_counter_columns.rb
@@ -0,0 +1,6 @@
+class DisallowNullOnCounterColumns < ActiveRecord::Migration[7.0]
+  def change
+    change_column :posts, :likes_count, :integer, default: 0, null: false
+    change_column :posts, :comments_count, :integer, default: 0, null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_12_115449) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_12_121441) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,8 +64,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_115449) do
     t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "likes_count"
-    t.integer "comments_count"
+    t.integer "likes_count", default: 0, null: false
+    t.integer "comments_count", default: 0, null: false
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
